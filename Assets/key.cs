0ae329c86a0792d887c7f2fd2aacc557df1e3f4c
devgonvarun <devgonvarun@gmail.com>
2017-03-06 20:41:40 +0530

    First Build

---
+++ Assets/key.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class key : MonoBehaviour {
+
+
+	public GameObject door;
+
+	public void OnKeyClicked(){
+		door.GetComponent<Door> ().Unlock ();
+		Destroy (gameObject);
+	}
+	}
---
+++ Assets/project/Scripts/Door.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Door : MonoBehaviour 
+{
+	bool locked = true;
+	bool closed = true;
+
+	void Update(){
+		if (!locked && !closed && transform.position.y < 23.2f) {
+			transform.Translate (0, 2.5f * Time.deltaTime, 0, Space.World);
+		}
+
+	}
+
+	public void Unlock()
+	{
+
+
+		locked = !locked;
+	}
+
+	public void openDoor(){
+		if (!locked) {
+			closed = !closed;
+		}
+
+
+
+	}
+	
+}
